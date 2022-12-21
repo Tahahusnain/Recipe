1bf6c67300c32d6fb18e30a1a181bb881f880082
Syed <taha.husnain20@gmail.com>
2022-12-21 20:30:28 +0500

    My first Commit

---
+++ src/main/java/spring/com/recipeapp/contollers/indexController.java
@@ -0,0 +1,4 @@
+package spring.com.recipeapp.contollers;
+
+public class indexController {
+}
---
+++ src/main/java/spring/com/recipeapp/domain/Notes.java
@@ -0,0 +1,40 @@
+package spring.com.recipeapp.domain;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue( strategy = GenerationType.SEQUENCE )
+    private Long id;
+    @OneToOne
+    private Recipe recipe;
+    private String recipeNotes;
+
+    public Recipe getRecipe ( ) {
+        return recipe;
+    }
+
+    public void setRecipe ( Recipe recipe ) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes ( ) {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes ( String recipeNotes ) {
+        this.recipeNotes = recipeNotes;
+    }
+
+
+
+    public Long getId ( ) {
+        return id;
+    }
+
+    public void setId ( Long id ) {
+        this.id = id;
+    }
+}
